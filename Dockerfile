db6b44f19a28d759b049f26a95b5ad22ec6e5942
EC2 Default User <ec2-user@ip-10-0-1-83.us-east-2.compute.internal>
2020-07-06 14:38:27 +0000

    updated files

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,20 @@ FROM ubuntu:latest
 ARG DEBIAN_FRONTEND=noninteractive
  
 ENV PGDATA=/var/lib/postgresql/data/pgdata
-ENV PG_HOST=0.0.0.0
+ENV PG_HOST=m5-ig-rds.cy7qnq8x0c88.us-east-2.rds.amazonaws.com
 ENV PG_PORT=5432
-ENV IG_DATABASE=image_gallery
+ENV IG_DATABASE=image-gallery
 ENV IG_USER=dongji
 ENV IG_PASSWD_FILE=cpsc4973
-
 
 RUN apt-get update -y && apt install default-jre -y 
 RUN mkdir /java-image-gallery
 COPY .  /java-image-gallery
 
 WORKDIR /java-image-gallery
+
+RUN chmod +x createDB.sh
+RUN createDB.sh
 
 CMD [ "java", "-jar", "java-image-gallery-all.jar"] 
 
--- createDB.sh
+++ createDB.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/bash
 
 
+USER=$IG_USER
+DATABASE=$IG_DATABASE
+HOST=$PG_HOST
+PW=$IG_PASSWD
+
+PGPASSWORD=$PW psql -h $HOST -U $USER -c "create table users (username varchar(100) not null primary key, password varchar(100), full_name varchar(200));"
+PGPASSWORD=$PW psql -h $HOST -U $USER -c "insert into users values ('dongji','cpsc4973','dongji - admin')";
+PGPASSWORD=$PW psql -h $HOST -U $USER -c "insert into users values ('jwinger','cpsc4973','Jefferey Winger - non admin')";
+PGPASSWORD=$PW psql -h $HOST -U $USER -c "insert into users values ('admin','admin','Administrator - admin')";
+
+PGPASSWORD=$PW psql -h $HOST -U $USER -c "select * from users";
 
